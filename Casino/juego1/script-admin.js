2623789783c7bafb23c69cb0292ca51c30d26719
annasuaareez <a.suarez.villaescusa@gmail.com>
2026-01-28 11:58:46 +0100

    CHANGES: Solucion problemas

--- Casino/juego1/script-admin.js
+++ Casino/juego1/script-admin.js
@@ -8,7 +8,7 @@ import {
   getDocs,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-import { app } from "./firebase.js";
+import { app } from "../../firebase.js";
 
 const db = getFirestore(app);
 
